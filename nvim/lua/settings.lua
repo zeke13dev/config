3c8b72330ae61f406adde766612d0101f574f4ad
Zeke Aharon Barnett <zbarnett@linux-01.andrew.cmu.edu>
2024-11-12 13:02:35 -0500

    poacker_compiled

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -14,3 +14,15 @@ vim.o.wrap = false
 
 -- Use system clipboard
 vim.o.clipboard = 'unnamedplus'
+
+-- Set indentation to 2 spaces for C files only
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
